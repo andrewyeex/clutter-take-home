b662866e31f6b1b816cd044ce46c382f3e19b46c
Andrew Yee <andrew.yee@mail.com>
2019-03-28 12:20:46 -0700

    set up testing

--- src/containers/main/main.test.js
+++ src/containers/main/main.test.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import App from './App'
+import Main from './Main'
 
 it('renders without crashing', () => {
-  shallow(<App />)
+  const wrapper = shallow(<Main />)
+  expect(wrapper).toExist()
 });
